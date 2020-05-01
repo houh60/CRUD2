6d1031eb07e26769db24d2880d58f109562e3456
William <C-William.Hou@charter.com>
2020-05-01 17:45:58 -0500

    new changes

--- src/app/employees/create-employee/create-employee.component.ts
+++ src/app/employees/create-employee/create-employee.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from 'src/app/models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,20 +10,43 @@ import { Department } from 'src/app/models/department.model';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  public photoPath = '';
+  set wholePath(p: string) {
+    this.photoPath = 'assets/images/' + p.substr(p.lastIndexOf('\\') + 1);
+  }
+
+  public previewPhoto = false;
+  public dateOfBirth: Date = new Date(2018, 0, 1);
+  public datePickerConfig: Partial<BsDatepickerConfig>;
+
   public departments: Department[] = [
-    { id: 1, name: 'Help Desk'},
-    { id: 2, name: 'HR'},
-    { id: 3, name: 'IT'},
-    { id: 4, name: 'Payroll'},
-    { id: 5, name: 'Admin'},
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' },
+    { id: 5, name: 'Admin' },
   ];
-  constructor() { }
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: true,
+        minDate: new Date(2019, 11, 1),
+        maxDate: new Date(2020, 4, 1),
+        dateInputFormat: 'DD/MM/YYYY'
+      }
+    );
+  }
 
   ngOnInit() {
   }
 
   public saveEmployee(empForm: NgForm): void {
     console.log(empForm);
+  }
+
+  public togglePhotoPreview(): void {
+    this.previewPhoto = !this.previewPhoto;
   }
 
 }
